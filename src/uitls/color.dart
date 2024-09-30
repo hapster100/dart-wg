be4a61715a419f8cc3b987a70e17b98405964d83
aland97 <aandreenko1997@gmail.com>
2024-09-30 16:03:00 +0700

    Color constructors

--- src/uitls/color.dart
+++ src/uitls/color.dart
@@ -7,6 +7,9 @@ class Color {
   Color.redN(int x) : this(x, 0, 0);
   Color.greenN(int x) : this(0, x, 0);
   Color.blueN(int x) : this(0, 0, x);
+  Color.yellowN(int x) : this(x, x, 0);
+  Color.purpleN(int x) : this(x, 0, x);
+  Color.cyanN(int x) : this(0, x, x);
   const Color.constC(this.r, this.g, this.b);
 
   static const Color white = Color.constC(255, 255, 255);
